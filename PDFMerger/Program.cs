ea91bdad86635f08fecadf3e8d7f7cb5dda19e21
Jasmin Backman <44293855+jasminbackman@users.noreply.github.com>
2019-10-15 13:36:09 +0300

    Initial commit - upload project files

---
+++ PDFMerger/Program.cs
@@ -0,0 +1,47 @@
+﻿using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PDFMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            DirectoryInfo dir = Directory.CreateDirectory("files");
+            DirectoryInfo outdir = Directory.CreateDirectory("output");
+            FileInfo[] files = dir.GetFiles().OrderBy(i => i.Name).ToArray();
+            List<PdfDocument> documents = new List<PdfDocument>();
+
+            foreach (var file in files)
+            {
+                if(file.Extension == ".pdf")
+                 documents.Add(PdfReader.Open(file.FullName, PdfDocumentOpenMode.Import));
+            }
+
+            using (PdfDocument outPdf = new PdfDocument())
+            {
+                foreach (var document in documents)
+                {
+                    CopyPages(document, outPdf);
+                }
+
+                outPdf.Save(outdir.FullName + "/output.pdf");
+            }
+
+            void CopyPages(PdfDocument from, PdfDocument to)
+            {
+                for (int i = 0; i < from.PageCount; i++)
+                {
+                    to.AddPage(from.Pages[i]);
+                }
+            }
+        }
+    }
+}
---
+++ PDFMerger/files/README.txt
@@ -0,0 +1,3 @@
+﻿Add PDF files you want to merge inside files folder.
+Run the program.
+Output file can be find from the output folder.
